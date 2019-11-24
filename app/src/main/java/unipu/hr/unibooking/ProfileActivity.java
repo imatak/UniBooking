b21b89cb4e5a37b5317186ddac06c6084596fc75
imatak <33956664+imatak@users.noreply.github.com>
2019-11-24 17:12:04 +0100

    Dodan kalendar i spinner za spremanje na bazu.

--- app/src/main/java/unipu/hr/unibooking/ProfileActivity.java
+++ app/src/main/java/unipu/hr/unibooking/ProfileActivity.java
@@ -15,19 +15,38 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
+    CalendarView kalendar;
+    Spinner razlog;
+    TextView userEmailSpremi;
+    EditText userText;
+    Button Spremi;
+
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
-
+    DatabaseReference reff;
+    Rezervacija rezervacija;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +56,64 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        kalendar = findViewById(R.id.kalendarRezervacije);
+        razlog = findViewById(R.id.razlogSpinner);
+        userEmailSpremi = findViewById(R.id.userEmailSpremi);
+        userText = findViewById(R.id.userText);
+        Spremi = findViewById(R.id.btnSave);
+
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Ispis kolegija");
+        categories.add("Upis na studij");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        razlog.setAdapter(dataAdapter);
+
+        razlog.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                // On selecting a spinner item
+                 rezervacija.setRazlog(parentView.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        });
+
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+        rezervacija = new Rezervacija();
+        reff = FirebaseDatabase.getInstance().getReference().child("Rezervacije");
+
+        Spremi.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view) {
+                //int a = Integer.parseInt(datum.getTex...)
+                // rezervacija.setDatum(a);
+                Calendar cl = Calendar.getInstance();
+                cl.setTimeInMillis(kalendar.getDate());
+                String date = "" + cl.get(Calendar.DAY_OF_MONTH) + "." + cl.get(Calendar.MONTH) + "." + cl.get(Calendar.YEAR);
+                //String time = "" + cl.get(Calendar.HOUR_OF_DAY) + ":" + cl.get(Calendar.MINUTE) + ":" + cl.get(Calendar.SECOND);
+
+                rezervacija.setDatum(date);
+                rezervacija.setEmailUsera(userEmailSpremi.getText().toString().trim());
+                rezervacija.setUserTekst(userText.getText().toString().trim());
+                reff.push().setValue(rezervacija);
+            }
+        });
+
+
+        userEmailSpremi.setText(firebaseUser.getEmail());
 
 
         drawer = findViewById(R.id.drawer_layout);
@@ -80,6 +154,5 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
             super.onBackPressed();
         }
     }
-
 
 }
---
+++ app/src/main/java/unipu/hr/unibooking/Rezervacija.java
@@ -0,0 +1,43 @@
+package unipu.hr.unibooking;
+
+public class Rezervacija {
+    private String EmailUsera;
+    private String UserTekst;
+    private String Datum;
+    private String Razlog;
+
+    public Rezervacija() {
+    }
+
+    public String getEmailUsera() {
+        return EmailUsera;
+    }
+
+    public void setEmailUsera(String emailUsera) {
+        EmailUsera = emailUsera;
+    }
+
+    public String getUserTekst() {
+        return UserTekst;
+    }
+
+    public void setUserTekst(String userTekst) {
+        UserTekst = userTekst;
+    }
+
+    public String getDatum() {
+        return Datum;
+    }
+
+    public void setDatum(String datum) {
+        Datum = datum;
+    }
+
+    public String getRazlog() {
+        return Razlog;
+    }
+
+    public void setRazlog(String razlog) {
+        Razlog = razlog;
+    }
+}
