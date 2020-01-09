cea18a10577c0e75860a171211c045fd48aff58e
imatak <33956664+imatak@users.noreply.github.com>
2020-01-09 17:53:49 +0100

    Session token - radnik update

--- app/src/main/java/unipu/hr/unibooking/Home.java
+++ app/src/main/java/unipu/hr/unibooking/Home.java
@@ -7,6 +7,9 @@ import android.provider.ContactsContract;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Home extends Application {
     @Override
     public void onCreate() {
@@ -17,10 +20,19 @@ public class Home extends Application {
 
         if(firebaseUser != null){
             //startActivity(new Intent(Home.this, ProfileActivity.class));
+            String email = firebaseUser.getEmail();
+            Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@unipu.hr");
+            Matcher mat = pattern.matcher(email);
 
-            Intent intent = new Intent(Home.this, ProfileActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            if(mat.matches()){
+                Intent intent = new Intent(Home.this, RadnikDashboardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }else{
+                Intent intent = new Intent(Home.this, ProfileActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
         }
     }
 }
